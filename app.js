722dd21228b3e9d935a8a2bdf1190a587cabaca9
Samuel <mastercraft9090@gmail.com>
2021-01-15 13:20:06 -0500

    new changes to app

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Hello World! Whwewewew'))
+app.get('/', (req, res) => res.send('Hello World! Where are we going to eat'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
